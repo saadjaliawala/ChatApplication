a31e7bd8330664a7899c4d0c6f143859fd997d1b
saadjaliawala <saadmehmood2012@gmail.com>
2020-07-10 17:35:39 +0500

    chat on going

--- src/Screens/ChatBox/index.js
+++ src/Screens/ChatBox/index.js
@@ -6,7 +6,9 @@ import {
     Text,
     StatusBar,
     Button,
-    Image
+    Image,
+    TextInput,
+    Alert
   } from 'react-native';
 
   
@@ -15,46 +17,230 @@ import {
   import {UserDetails} from '../../redux/actions/UserDetails.js';
   import styles from './style.js';
   import Header from '../../components/Header/index.js';
+  import Entypo from 'react-native-vector-icons/Entypo';
+  import Ionicons from 'react-native-vector-icons/Ionicons';
+  import firestore from '@react-native-firebase/firestore';
+  // import styles from './style.js';
 
 
 
 const ChatBoxScreen = (props) => {
 
+    const [ChatIdBool , SetChatIdBool] = useState();
+    const [UidBool , SetUidBool] = useState();
     const [ChatUser , SetChatUser] = useState();
     const [CurrentUser , SetCurrentUser] = useState();
+    const [Textvalue, onChangeText] = useState();
+    const [PushedKey , SetPushedKey] = useState();
+    const [UserMessages , SetUserMessages ] = useState([]);
 
     useEffect(() => {
-        console.log("chat user" , props.route.params);
+        // console.log("chat user" , props.route.params);
+        const CurrentChatUserInApp = props.route.params;
         SetChatUser( props.route.params);
-
+        const CurrentUserInApp = store.getState().UserDetails;
         SetCurrentUser(store.getState().UserDetails);
+
+
+
+         firestore()
+   .collection('Users')
+   .doc(CurrentUserInApp.user?.uid)
+   .onSnapshot(Data => {
+    //  console.log(Data);
+     if(Data._data?.ChatId)
+     {
+       SetChatIdBool(true);
+      Data._data.ChatId.map((ddata) => {
+        // console.log(ddata);
+        if(ddata.uid == CurrentChatUserInApp?.users?.uid )
+        {
+          // alert("SaadMEHMOOD");
+          SetPushedKey(ddata.pushKey);
+          SetUidBool(true);
+          let array = [];
+          firestore()
+        .collection('Chat')
+          .doc(ddata.pushKey)
+          .collection('Messages')
+          .get()
+          .then(querySnaphot => {
+            // console.log(querySnaphot);
+            querySnaphot.forEach(Data => {
+             array.push({message: Data?._data?.message , 
+            senderUid: Data?._data?.senderUid
+            })
+            console.log(array);
+              // console.log(Data);
+            })
+          })
+          
+
+        }
+        else {
+          SetUidBool(false);
+        }
+      })
+     }
+     else {
+       alert("Saad");
+     }
+   })   
+
+
+
+ 
+
+
  },[])
 
+
+ const SendButtonPress = async () => {
+  //  const messages = {
+  //   message: Textvalue,
+  //   senderUid: CurrentUser.user?.uid ,
+  //   senderName: CurrentUser.user?.displayName,
+  //  }
+
+   if(!ChatIdBool)
+   {
+  
+   const pushKey = await firestore()
+   .collection('Chat')
+
+    .add({ })
+
+   firestore()
+   .collection('Chat')
+   .doc(pushKey._documentPath?._parts[1]) 
+   .collection('Messages')
+   .add({
+     message: Textvalue,
+    senderUid: CurrentUser.user?.uid ,
+    senderName: CurrentUser.user?.displayName,
+  })
+
+  firestore()
+  .collection('Users')
+  .doc(CurrentUser?.user?.uid)
+  
+  .update({
+    ChatId: firestore.FieldValue.arrayUnion({ uid: ChatUser.users?.uid , name: ChatUser.users?.name ,
+       pushKey: pushKey._documentPath?._parts[1] })
+  })
+
+  firestore()
+  .collection('Users')
+  .doc(ChatUser.users?.uid)
+  .update({
+    ChatId: firestore.FieldValue.arrayUnion ({ uid: CurrentUser.user?.uid , 
+      name: CurrentUser.user?.displayName , pushKey: pushKey._documentPath?._parts[1] })
+    
+  })
+
+}
+if(UidBool)
+{
+  alert("booltrue");
+  firestore()
+  .collection('Chat')
+  .doc(PushedKey)
+  .collection('Messages')
+  .add({
+    message: Textvalue,
+    senderUid: CurrentUser.user?.uid ,
+    senderName: CurrentUser.user?.displayName,
+  })
+}  
+else {
+
+
+  const pushKey = await firestore()
+  .collection('Chat')
+
+   .add({ })
+
+  firestore()
+  .collection('Chat')
+  .doc(pushKey._documentPath?._parts[1]) 
+  .collection('Messages')
+  .add({
+    message: Textvalue,
+   senderUid: CurrentUser.user?.uid ,
+   senderName: CurrentUser.user?.displayName,
+ })
+
+ firestore()
+ .collection('Users')
+ .doc(CurrentUser?.user?.uid)
+ 
+ .update({
+   ChatId: firestore.FieldValue.arrayUnion({ uid: ChatUser.users?.uid , name: ChatUser.users?.name ,
+      pushKey: pushKey._documentPath?._parts[1] })
+ })
+
+ firestore()
+ .collection('Users')
+ .doc(ChatUser.users?.uid)
+ .update({
+   ChatId: firestore.FieldValue.arrayUnion ({ uid: CurrentUser.user?.uid , 
+     name: CurrentUser.user?.displayName , pushKey: pushKey._documentPath?._parts[1] })
+   
+ })
+
+
+}
+
+
+
+   
+  }
+
+
     const _renderFunction = () => {
-      console.log("current user" , CurrentUser);
+      // console.log("current user" , CurrentUser);
         return(
-            <View>
-              {/* <Text>ChatBox Screen</Text> */}
-              {/* <Image 
-              source= {{ uri: ChatUser.users?.phot }}
-              /> */}
+            <View> 
          {ChatUser && <Header 
          props = {props}
           ChatUser={ChatUser}
           />}
-        {/* {ChatUser && <Image 
-        source={{ uri: ChatUser.users?.photoUrl }}
-        style={styles.ChatUserImage}
-        />}
-              
-        {ChatUser&&<Text>{ChatUser.users?.name}</Text>} */}
-       
-
             </View>
           )}
 
+         const _renderInput = () => {
+           return(
+             <View style={ styles.InputViewParent } >
+               <Entypo 
+               name= "emoji-happy"
+               size= {25}
+               color = 'grey' 
+               style={{ paddingLeft: 10 }}
+               />
+                <TextInput 
+          onChangeText =  {(text) => onChangeText(text) } 
+          multiline={true}
+          style= {styles.InputStyle}
+          placeholder= "Type a message"
+          // value={value}
+          />
+          <Ionicons 
+          name="send"
+          size = {25}
+          color= 'blue'
+          style={styles.sendStyle } 
+          onPress={() => SendButtonPress() }
+          />
+             </View>
+           )}
+
     return(
-       _renderFunction()
+      <View style={{flex: 1 ,  borderWidth: 5}} >
+
+        { _renderFunction()}
+        { _renderInput()}
+      </View>
+      
     )}
 
   export default ChatBoxScreen;
--- src/Screens/ChatBox/style.js
+++ src/Screens/ChatBox/style.js
@@ -1,9 +1,49 @@
 import {StyleSheet} from 'react-native';
-import style from '../Users/style';
+// import {StyleSheet} from 'react-native';
 
 export default styles = StyleSheet.create({
     ChatUserImage: {
         height: 50,
         width: 50
+    },
+    InputViewParent: {
+        position: 'absolute' ,
+         bottom: 0 ,
+        //  padding: 5,
+         margin: '1%',
+          width: '98%' ,
+           flex: 1 ,
+        //    height: 50,
+           borderRadius: 20,
+           borderWidth: 1,
+           borderColor: 'lightgrey',
+           flexDirection: 'row',
+           alignItems: 'center',
+           minHeight: 50,
+
+    },
+
+    InputStyle: {
+        // height: 40,
+        // borderWidth: 1,
+        // borderBottomWidth: 1,
+        // marginBottom: 5,
+        color: 'grey',
+        paddingLeft: 20,
+        width: '80%',
+        // minHeight: 80,
+        // textColor: 'lightgrey'
+        
+        // width: '80%',
+        // marginHorizontal: '5%'
+        // alignItems: 'flex-end'     
+           // alignSelf: "flex-end",
+        // justifyContent: 'flex-end',
+        // marginTop: '80%'
+        // position: "absolute",
+        // bottom: 0
+    },
+    sendStyle: {
+        
     }
 })
